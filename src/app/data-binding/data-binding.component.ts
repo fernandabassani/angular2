8eecec3d1dc2dbe8dafdca8bc24da462cc678147
fernandabassani <nandabassani1@outlook.com>
2018-02-25 20:01:26 -0300

    init

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+url: string= 'http://fernanda.com';
+cursoAngular: boolean = true;
+urlImagem = 'http://lorempixel.com/400/200/sports/';
+
+
+
+getValor(){
+  return 1;
+}
+
+
+getCurtirCurso(){
+  return true;
+}
+
+botaoClicado(){
+  alert('Botaoclicado!');
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
